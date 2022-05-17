34cc1cbc6359bb027aedac73ae4f2412b224adc7
Mani Moayedi <acidmanic.moayedi@gmail.com>
2022-05-17 13:12:47 +0430

    Add seed uildupscript and crud buildup scripts

---
+++ Meadow.Test.Functional/Scripts/0002-add-cruds.sql
@@ -0,0 +1,31 @@
+
+
+create procedure spGetAllPersons
+as
+    select * from Persons
+go
+
+create procedure spGetPersonById(@Id bigint)
+as
+    select * from Persons where Id = @Id
+go
+
+create procedure spGetAllEager
+as
+    select * from Persons join Jobs J on J.Id = Persons.JobId
+go
+
+create procedure spGetPersonByIdEager(@Id bigint)
+as
+    select * from Persons P join Jobs J on J.Id = P.JobId where P.Id = @Id
+go
+
+create procedure spGetAllJobs
+as 
+    select * from Jobs
+go
+
+create procedure spGetJobById(@Id bigint)
+as
+    select * from Jobs where Id = @Id
+go
