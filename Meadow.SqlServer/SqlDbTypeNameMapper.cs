ba03d56c1697600edded093780a3a568df366766
Mani Moayedi <acidmanic.moayedi.com>
2023-02-16 04:41:16 +0330

    Move SqlDbTypeNameMapper to it's own project

--- Meadow.SqlServer/SqlDbTypeNameMapper.cs
+++ Meadow.SqlServer/SqlDbTypeNameMapper.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using Meadow.DataTypeMapping;
 
-namespace Meadow.DataTypeMapping
+namespace Meadow.SqlServer
 {
     public class SqlDbTypeNameMapper : SqlDbTypeNameMapperBase
     {
