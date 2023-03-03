b6f5f4d806da68e595cb019d34ff39ae2f5e7d14
Mani Moayedi <acidmanic.moayedi.com>
2023-03-03 13:25:43 +0330

    Update ms-sqls-server's dbTypeMapper to support Guid

--- Meadow.SqlServer/SqlDbTypeNameMapper.cs
+++ Meadow.SqlServer/SqlDbTypeNameMapper.cs
@@ -25,7 +25,7 @@ namespace Meadow.SqlServer
             _typeMap[typeof(bool)] = SqlDbType.Bit.ToString().ToLower();
             _typeMap[typeof(string)] = SqlDbType.NVarChar.ToString().ToLower() + "(256)";
             _typeMap[typeof(char)] = SqlDbType.NChar.ToString().ToLower() + "(1)";
-            _typeMap[typeof(Guid)] = SqlDbType.VarChar.ToString().ToLower() + "(40)";
+            _typeMap[typeof(Guid)] = SqlDbType.UniqueIdentifier.ToString().ToLower();
             _typeMap[typeof(DateTime)] = SqlDbType.DateTime.ToString().ToLower();
             _typeMap[typeof(DateTimeOffset)] = SqlDbType.DateTimeOffset.ToString().ToLower();
             _typeMap[typeof(byte[])] = SqlDbType.Binary.ToString().ToLower();
@@ -42,7 +42,7 @@ namespace Meadow.SqlServer
             _typeMap[typeof(decimal?)] = SqlDbType.Decimal.ToString().ToLower();
             _typeMap[typeof(bool?)] = SqlDbType.Bit.ToString().ToLower();
             _typeMap[typeof(char?)] = SqlDbType.NChar.ToString().ToLower() + "(1)";
-            _typeMap[typeof(Guid?)] = SqlDbType.VarChar.ToString().ToLower() + "(40)";
+            _typeMap[typeof(Guid?)] = SqlDbType.UniqueIdentifier.ToString().ToLower();
             _typeMap[typeof(DateTime?)] = SqlDbType.DateTime.ToString().ToLower();
             _typeMap[typeof(DateTimeOffset?)] = SqlDbType.DateTimeOffset.ToString().ToLower();
             //_typeMap[typeof(System.Data.Linq.Binary)] = DbType.Binary;   
--- Meadow/Scaffolding/Macros/MacroBase.cs
+++ Meadow/Scaffolding/Macros/MacroBase.cs
@@ -35,4 +35,5 @@ public abstract class MacroBase : IMacro
 
         return type;
     }
+    
 }
