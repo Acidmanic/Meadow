a7e162492b483621b3aba1dca82d50f37bc97eca
Mani Moayedi <acidmanic.moayedi.com>
2023-03-07 21:27:00 +0330

    Fix altered type issue by updating relection package

--- Meadow.Test.Functional/Program.cs
+++ Meadow.Test.Functional/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            new Tdd37FlagEnumIs().Main();
+            new Tdd38AlteredTypeBugCase().Main();
         }
     }
 }
---
+++ Meadow.Test.Functional/Tdd38AlteredTypeBugCase.cs
@@ -0,0 +1,14 @@
+using Meadow.Test.Functional.TDDAbstractions;
+
+namespace Meadow.Test.Functional
+{
+    public class Tdd38AlteredTypeBugCase : MeadowFunctionalTest
+    {
+
+        public override void Main()
+        {
+
+
+        }
+    }
+}
