b2abb994f7c435f909714429ee79ac9a2fc84e8a
Mani Moayedi <acidmanic.moayedi@gmail.com>
2023-10-04 13:22:20 +0330

    re-test un-indexed-problem for all databases

--- Meadow.Test.Functional/Tdd41FTestFilterLogicBeingCorrect.cs
+++ Meadow.Test.Functional/Tdd41FTestFilterLogicBeingCorrect.cs
@@ -16,7 +16,7 @@ namespace Meadow.Test.Functional
     {
         protected override void SelectDatabase()
         {
-            UseSqLite();
+            UsePostgre();
         }
 
         protected override void Main(MeadowEngine engine, ILogger logger)
