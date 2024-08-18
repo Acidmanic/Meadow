dfd879a951b2722bd00dd61a01774747cb1e7d68
Mani Moayedi <acidmanic.moayedi@gmail.com>
2024-08-18 16:25:27 +0330

    complete find-paged test coverage for fulltree accesses

--- Meadow.Test.Functional/Suits/FindPagedSuit.cs
+++ Meadow.Test.Functional/Suits/FindPagedSuit.cs
@@ -20,26 +20,26 @@ public class FindPagedSuit
     [InlineData(true)]
     [InlineData(false)]
     public void Must_Filter_Persons_OlderThan50(bool fullTree) =>
-        FindPagedMustFilterRecords(qb => qb.Where(p => p.Age).IsLargerThan(50), p => p.Age > 50,fullTree);
+        FindPagedMustFilterRecords(qb => qb.Where(p => p.Age).IsLargerThan(50), p => p.Age > 50, fullTree);
 
     [Theory]
     [InlineData(true)]
     [InlineData(false)]
     public void Must_Filter_Persons_YoungerThan50(bool fullTree) =>
-        FindPagedMustFilterRecords(qb => qb.Where(p => p.Age).IsSmallerThan(50), p => p.Age < 50,fullTree);
+        FindPagedMustFilterRecords(qb => qb.Where(p => p.Age).IsSmallerThan(50), p => p.Age < 50, fullTree);
 
     [Theory]
     [InlineData(true)]
     [InlineData(false)]
     public void Must_Filter_Persons_Named_Mani(bool fullTree) =>
-        FindPagedMustFilterRecords(qb => qb.Where(p => p.Name).IsEqualTo("Mani"), p => p.Name == "Mani",fullTree);
+        FindPagedMustFilterRecords(qb => qb.Where(p => p.Name).IsEqualTo("Mani"), p => p.Name == "Mani", fullTree);
 
     [Theory]
     [InlineData(true)]
     [InlineData(false)]
     public void Must_Filter_Persons_Named_Mina_OR_Farshid(bool fullTree) =>
         FindPagedMustFilterRecords(qb => qb.Where(p => p.Name).IsEqualTo("Mina", "Farshid"),
-            p => p.Name == "Mina" || p.Name == "Farshid",fullTree);
+            p => p.Name == "Mina" || p.Name == "Farshid", fullTree);
 
 
     [Theory]
@@ -47,7 +47,7 @@ public class FindPagedSuit
     [InlineData(false)]
     public void Must_Filter_Persons_NOT_Named_Mina_NOR_Farshid(bool fullTree) =>
         FindPagedMustFilterRecords(qb => qb.Where(p => p.Name).IsNotEqualTo("Mina", "Farshid"),
-            p => p.Name != "Mina" && p.Name != "Farshid",fullTree);
+            p => p.Name != "Mina" && p.Name != "Farshid", fullTree);
 
 
     [Theory]
@@ -59,10 +59,10 @@ public class FindPagedSuit
 
         env.Perform(Databases, e =>
         {
-            var all = e.FindPaged<Person>(fullTree:fullTree).FromStorage;
+            var all = e.FindPaged<Person>(fullTree: fullTree).FromStorage;
 
-            AssertX.ContainSameItems(e.Data.Get<Person>(p => true).ToList(), 
-                all,_personIdentifier, true,fullTree);
+            AssertX.ContainSameItems(e.Data.Get<Person>(p => true).ToList(),
+                all, _personIdentifier, true, fullTree);
 
             for (int size = 1; size < all.Count; size++)
             {
@@ -70,60 +70,77 @@ public class FindPagedSuit
                 {
                     var expected = all.Skip(offset).Take(size).ToList();
 
-                    var actual = e.FindPaged<Person>(offset: offset, size: size,fullTree:fullTree).FromStorage;
+                    var actual = e.FindPaged<Person>(offset: offset, size: size, fullTree: fullTree).FromStorage;
 
-                    AssertX.ContainSameItems(expected, actual,_personIdentifier,true,fullTree);
+                    AssertX.ContainSameItems(expected, actual, _personIdentifier, true, fullTree);
                 }
             }
         });
     }
 
-    [Fact]
-    public void Must_Sort_By_Age_Ascending()
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Must_Sort_By_Age_Ascending(bool fullTree)
         => FindPagedMustPerformCorrectSorting((p1, p2) => p1.Age - p2.Age,
-            o => o.OrderAscendingBy(p => p.Age));
+            o => o.OrderAscendingBy(p => p.Age), fullTree);
 
-    [Fact]
-    public void Must_Sort_By_Age_Descending()
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Must_Sort_By_Age_Descending(bool fullTree)
         => FindPagedMustPerformCorrectSorting((p1, p2) => p2.Age - p1.Age,
-            o => o.OrderDescendingBy(p => p.Age));
-
-
-    [Fact]
-    public void Must_Sort_By_Name_Ascending()
-        => FindPagedMustPerformCorrectSorting((p1, p2) => string.Compare(p1.Name, p2.Name, StringComparison.Ordinal),
-            o => o.OrderAscendingBy(p => p.Name));
-
-    [Fact]
-    public void Must_Sort_By_Name_Descending()
-        => FindPagedMustPerformCorrectSorting((p1, p2) => string.Compare(p2.Name, p1.Name, StringComparison.Ordinal),
-            o => o.OrderDescendingBy(p => p.Name));
-
-
-    [Fact]
-    public void Must_Find_ByPortionOf_SurName()
-        => FindPagedMustFindExpectedItemsForGivenSearchTerms(p =>
-            p.Name == "Mani" ||
-            p.Name == "Mona" ||
-            p.Name == "Farshid", "Moai");
-
-    [Fact]
-    public void Must_Find_ByPortionOf_Name()
-        => FindPagedMustFindExpectedItemsForGivenSearchTerms(p =>
-            p.Name == "Farshid" ||
-            p.Name == "Farimehr", "far");
-
-    [Fact]
-    public void Must_Find_Name_IgnoreCase()
-        => FindPagedMustFindExpectedItemsForGivenSearchTerms(p =>
-            p.Name == "Farshid", "farshid");
+            o => o.OrderDescendingBy(p => p.Age), fullTree);
 
 
     [Theory]
-    [InlineData("Mina")]
-    [InlineData("Mani")]
-    [InlineData("Mona")]
-    public void Must_Find_Only_UnDeletedResults(string deleteeName)
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Must_Sort_By_Name_Ascending(bool fullTree)
+        => FindPagedMustPerformCorrectSorting((p1, p2) => string.Compare(p1.Name, p2.Name, StringComparison.Ordinal),
+            o => o.OrderAscendingBy(p => p.Name), fullTree);
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Must_Sort_By_Name_Descending(bool fullTree)
+        => FindPagedMustPerformCorrectSorting((p1, p2) => string.Compare(p2.Name, p1.Name, StringComparison.Ordinal),
+            o => o.OrderDescendingBy(p => p.Name), fullTree);
+
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Must_Find_ByPortionOf_SurName(bool fullTree)
+        => FindPagedMustFindExpectedItemsForGivenSearchTerms(p =>
+            p.Name == "Mani" ||
+            p.Name == "Mona" ||
+            p.Name == "Farshid", fullTree, "Moai");
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Must_Find_ByPortionOf_Name(bool fullTree)
+        => FindPagedMustFindExpectedItemsForGivenSearchTerms(p =>
+            p.Name == "Farshid" ||
+            p.Name == "Farimehr", fullTree, "far");
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Must_Find_Name_IgnoreCase(bool fullTree)
+        => FindPagedMustFindExpectedItemsForGivenSearchTerms(p =>
+            p.Name == "Farshid", fullTree, "farshid");
+
+
+    [Theory]
+    [InlineData("Mina", false)]
+    [InlineData("Mina", true)]
+    [InlineData("Mani", false)]
+    [InlineData("Mani", true)]
+    [InlineData("Mona", false)]
+    [InlineData("Mona", true)]
+    public void Must_Find_Only_UnDeletedResults(string deleteeName, bool fullTree)
     {
         var env = new Environment<PersonsDataProvider>();
 
@@ -136,14 +153,14 @@ public class FindPagedSuit
 
             var expectedResult = e.Data.Get<Person>(p => p.Name != deleteeName).ToList();
 
-            var found = e.FindPaged<Person>().FromStorage;
+            var found = e.FindPaged<Person>(fullTree:fullTree).FromStorage;
 
-            AssertX.ContainSameItemsShallow(expectedResult, found, _personIdentifier);
+            AssertX.ContainSameItems(expectedResult, found, _personIdentifier,true,fullTree);
         });
     }
 
 
-    private void FindPagedMustFindExpectedItemsForGivenSearchTerms(Func<Person, bool> predicate, params string[] searchTerms)
+    private void FindPagedMustFindExpectedItemsForGivenSearchTerms(Func<Person, bool> predicate, bool fullTree, params string[] searchTerms)
     {
         var env = new Environment<PersonsDataProvider>();
 
@@ -155,9 +172,9 @@ public class FindPagedSuit
 
             var expectedResult = e.Data.Get(predicate).ToList();
 
-            var found = e.FindPaged<Person>(searchTerms: terms).FromStorage;
+            var found = e.FindPaged<Person>(searchTerms: terms, fullTree: fullTree).FromStorage;
 
-            AssertX.ContainSameItemsShallow(expectedResult, found, _personIdentifier);
+            AssertX.ContainSameItems(expectedResult, found, _personIdentifier, true, fullTree);
         });
     }
 
@@ -171,26 +188,26 @@ public class FindPagedSuit
             var found = e.FindPaged(qb, fullTree: fullTree).FromStorage;
 
             AssertX.ContainSameItems(expectedItems: e.Data.Get(predicate).ToList(),
-                actualItems:found, 
+                actualItems: found,
                 toString: _personIdentifier,
-                ignoreId:true,
-                deepCompare:fullTree);
+                ignoreId: true,
+                deepCompare: fullTree);
         });
     }
 
-    private void FindPagedMustPerformCorrectSorting(Comparison<Person> expectedComparer, Action<OrderSetBuilder<Person>> buildActualOrders)
+    private void FindPagedMustPerformCorrectSorting(Comparison<Person> expectedComparer, Action<OrderSetBuilder<Person>> buildActualOrders, bool fullTree)
     {
         var env = new Environment<PersonsDataProvider>();
 
         env.Perform(Databases, e =>
         {
-            var found = e.FindPaged(order: buildActualOrders).FromStorage;
+            var found = e.FindPaged(order: buildActualOrders, fullTree: fullTree).FromStorage;
 
             var expected = e.Data.Get(expectedComparer);
 
             AssertX.AreSameSize(expected, found, "Read items does not match with expectations");
 
-            AssertX.AreInSameOrderShallow(expected, found);
+            AssertX.AreInSameOrder(expected, found, _personIdentifier, true, fullTree);
         });
     }
 }
