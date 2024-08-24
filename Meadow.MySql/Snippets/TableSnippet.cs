a4ec5ec92c7f33aa50a1c9f05823a321ad50345c
Mani Moayedi <acidmanic.moayedi@gmail.com>
2024-08-24 16:32:12 +0330

    Add Table Snippet for MySql

---
+++ Meadow.MySql/Snippets/TableSnippet.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using Meadow.Scaffolding.Attributes;
+using Meadow.Scaffolding.Snippets;
+using Meadow.Scaffolding.Snippets.Builtin;
+
+namespace Meadow.MySql.Snippets;
+
+[CommonSnippet(CommonSnippets.CreateTable)]
+public class TableSnippet : ISnippet
+{
+    public SnippetToolbox? Toolbox { get; set; }
+
+
+    public string CreateTablePhrase => Toolbox?.SqlTranslator.CreateTablePhrase(
+        Toolbox.Configurations.RepetitionHandling,
+        Toolbox.ProcessedType.NameConvention.TableName) ?? string.Empty;
+
+    public string Parameters => string.Join(",\n\t\t",Toolbox?.ProcessedType.Parameters.Select(Toolbox.SqlTranslator.TableColumnDefinition).ToList() ?? new List<string>());
+
+    public ISnippet Line => new CommentLineSnippet();
+    
+    public string Template => @"
+{CreateTablePhrase}({Parameters});
+{Line}";
+}
