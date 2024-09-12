e7b817021909bbb69e9cf310a44072748de7cffe
Mani Moayedi <acidmanic.moayedi@gmail.com>
2024-09-12 10:30:40 +0330

    Update SaveSnippet to declare procedcure using snippetFunctions

--- Meadow.SQLite/Snippets/SaveProcedureByCollectionNameSnippet.cs
+++ Meadow.SQLite/Snippets/SaveProcedureByCollectionNameSnippet.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using Meadow.Contracts;
 using Meadow.Scaffolding.CodeGenerators.CodeGeneratingComponents;
+using Meadow.Scaffolding.Extensions;
 using Meadow.Scaffolding.Macros.BuiltIn.Snippets;
 using Meadow.Scaffolding.Snippets;
 
@@ -8,23 +9,20 @@ namespace Meadow.SQLite.Snippets;
 
 public class SaveProcedureByCollectionNameSnippet : ISnippet
 {
-    public ISnippetToolbox Toolbox { get; set; } 
+    public ISnippetToolbox Toolbox { get; set; }
+
+    private readonly SaveProcedureComponents _components;
 
     public SaveProcedureByCollectionNameSnippet(ISnippetToolbox toolbox, SaveProcedureComponents saveComponents)
     {
         Toolbox = toolbox;
+        _components = saveComponents;
 
-        KeyProcedureCreationHeader = toolbox.SqlTranslator.CreateProcedurePhrase
-            (toolbox.Configurations.RepetitionHandling, saveComponents.ProcedureName);
-        
         PreKeyWhereClause = toolbox.ParameterNameValueSetJoint(saveComponents.WhereEqualities, " AND ", "@");
         PreKeyInsertColumns = string.Join(',', saveComponents.InsertUpdateParameters.Select(p => p.Name));
         PreKeyInsertValues = string.Join(',', saveComponents.InsertUpdateParameters.Select(p => "@" + p.Name));
         PreKeyUpdates = toolbox.ParameterNameValueSetJoint(saveComponents.InsertUpdateParameters, ",", "@");
-        KeyParameters = toolbox.ParameterNameTypeJoint(toolbox.ProcessedType.Parameters, ",", "@");
     }
-
-    public string KeyProcedureCreationHeader { get; }
     
     public string PreKeyWhereClause { get; }
 
@@ -36,13 +34,17 @@ public class SaveProcedureByCollectionNameSnippet : ISnippet
 
     public string KeyTableName => Toolbox.ProcessedType.NameConvention.TableName;
 
-    public string KeyEntityFilterSegment => Toolbox.GetEntityFiltersWhereClause( " AND ", " ");
-
-    public string KeyParameters { get; }
+    public string KeyEntityFilterSegment => Toolbox.GetEntityFiltersWhereClause(" AND ", " ");
+    
+    public string Procedure(string content) => Toolbox.Procedure(
+        RepetitionHandling.Create,
+        _components.ProcedureName,
+        content,
+        string.Empty, Toolbox.ProcessedType.NameConvention.TableName,
+        Toolbox.ProcessedType.Parameters.ToArray());
 
     public string Template => @"
-{KeyProcedureCreationHeader}({KeyParameters}) AS
-
+{Procedure}
     BEGIN;
     PRAGMA temp_store = 2;
     CREATE TEMP TABLE _alreadyExists(Id INTEGER PRIMARY KEY);
@@ -55,6 +57,6 @@ public class SaveProcedureByCollectionNameSnippet : ISnippet
     SELECT * FROM {KeyTableName} WHERE (({PreKeyWhereClause}) OR ROWID = LAST_INSERT_ROWID()) {KeyEntityFilterSegment} LIMIT 1;
     DROP TABLE _alreadyExists;
     END;
-GO
+{/Procedure}
 ".Trim();
 }
