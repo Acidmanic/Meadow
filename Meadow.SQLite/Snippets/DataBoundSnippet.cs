a7a5400b0a9bbe74716362cba1e207518e3a387d
Mani Moayedi <acidmanic.moayedi@gmail.com>
2024-09-17 16:35:56 +0330

    remove invalid interpolation sign

--- Meadow.SQLite/Snippets/DataBoundSnippet.cs
+++ Meadow.SQLite/Snippets/DataBoundSnippet.cs
@@ -38,5 +38,5 @@ public class DataBoundSnippet:ISnippet
         return new List<ISnippet>();
     }
 
-    public string Template => $"{EntityDataBounds}";
+    public string Template => "{EntityDataBounds}";
 }
