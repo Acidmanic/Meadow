4c511283b47a5d2dadb16ccd72fd8902c78679b0
Mani Moayedi <acidmanic.moayedi@gmail.com>
2024-09-21 23:24:51 +0330

    Add parameter injection to direct requests, add insert translation

--- Meadow.Test.Functional/TestEnvironment/Extensions/SuitContextExtensions.cs
+++ Meadow.Test.Functional/TestEnvironment/Extensions/SuitContextExtensions.cs
@@ -1,21 +1,22 @@
+using System.Diagnostics.CodeAnalysis;
+using Acidmanic.Utilities.Filtering.Extensions;
+using Acidmanic.Utilities.Reflection.ObjectTree;
 using Meadow.DataAccessResolving;
 using Meadow.Extensions;
 using Meadow.Sql.Extensions;
 
 namespace Meadow.Test.Functional.TestEnvironment.Extensions;
 
-public static  class SuitContextExtensions
+public static class SuitContextExtensions
 {
-    
     public static string TranslateSelectAll<TModel>(this ISuitContext context, bool fullTree = false)
     {
-
         var configuration = context.MeadowConfiguration;
 
         var resolver = new DataAccessServiceResolver(configuration);
 
         var tr = resolver.SqlTranslator;
-        
+
         var semi = tr.UsesSemicolon ? ";" : string.Empty;
 
         var nc = configuration.GetNameConvention(typeof(TModel));
@@ -25,5 +26,53 @@ public static  class SuitContextExtensions
         source = tr.GetQuoters().QuoteTableName(source);
 
         return $"SELECT * FROM {source}{semi}";
+    }
+
+    public static string TranslateInsert<TModel>(this ISuitContext context, TModel model) where TModel : class
+    {
+        var configuration = context.MeadowConfiguration;
+
+        var resolver = new DataAccessServiceResolver(configuration);
+
+        var sqlTranslator = resolver.SqlTranslator;
+        var valueTranslator = resolver.ValueTranslator;
+
+        var semi = sqlTranslator.UsesSemicolon ? ";" : string.Empty;
+
+        var type = typeof(TModel);
+
+        var nc = configuration.GetNameConvention(type);
+
+        var qt = sqlTranslator.GetQuoters();
+        
+        var source = qt.QuoteTableName(nc.TableName);
+        
+        var ev = new ObjectEvaluator(model);
+
+        var flatten = ev.ToStandardFlatData(o => o.ExcludeNulls().FullTree().UseAlternativeTypes());
+
+        var fullTreeMap = configuration.GetFullTreeMap(type);
+
+        var columns = new List<string>();
+        var values = new List<string>();
+
+        
+        foreach (var dp in flatten)
+        {
+            var name = ev.Map.FieldKeyByAddress(dp.Identifier).Headless().ToString();
+
+            var parameterName = fullTreeMap.GetColumnName(name);
+
+            if (parameterName)
+            {
+                var value = valueTranslator.Translate(dp.Value);
+                
+                columns.Add(qt.QuoteColumnName(parameterName));
+                
+                values.Add(value);
+            }
+        }
+
+        return $"INSERT INTO {source} ({string.Join(',',columns)}) VALUES ({string.Join(',',values)}){semi}";
     }
 }
--- Meadow/Requests/BuiltIn/DiscouragedDirectSqlRequest.cs
+++ Meadow/Requests/BuiltIn/DiscouragedDirectSqlRequest.cs
@@ -1,27 +1,64 @@
+using Acidmanic.Utilities.Filtering.Extensions;
+using Acidmanic.Utilities.Reflection.ObjectTree;
+using Meadow.Extensions;
+using Meadow.Sql.Extensions;
+
 namespace Meadow.Requests.BuiltIn;
 
-public class DiscouragedDirectSqlRequest<TReturn>:MeadowRequest<MeadowVoid,TReturn> where TReturn : class
+public class DiscouragedDirectSqlRequest<TReturn> : MeadowRequest<MeadowVoid, TReturn> where TReturn : class
 {
-    private readonly string _sql;
-    
-    public DiscouragedDirectSqlRequest(string sql) : base(true)
+    private string _executingSql;
+
+    public DiscouragedDirectSqlRequest(string sql, object? parameters = null) : base(true)
     {
-        _sql = sql;
+        _executingSql = sql;
+
+        if (parameters is { } p)
+        {
+            Setup(context => { _executingSql = Inject(context, sql, p); });
+        }
+
         Execution = RequestExecution.RequestTextIsExecutable;
+    }
+
+    private string Inject(RequestContext context, string sql, object parameters)
+    {
+        var ev = new ObjectEvaluator(parameters);
+
+        var flatten = ev.ToStandardFlatData(o => o.ExcludeNulls().FullTree().UseAlternativeTypes());
+
+        var fullTreeMap = context.Configuration.GetFullTreeMap(parameters.GetType());
+        
+        foreach (var dp in flatten)
+        {
+            var name = ev.Map.FieldKeyByAddress(dp.Identifier).Headless().ToString();
+
+            var value = context.ValueTranslator.Translate(dp.Value);
+
+            sql = sql.Replace("{" + name + "}", value);
+
+            var parameterName = fullTreeMap.GetColumnName(name);
+
+            if (parameterName)
+            {
+                sql = sql.Replace(parameterName, value);
+            }
+        }
+
+        return sql;
     }
 
     public override string RequestText
     {
-        get => _sql;
+        get => _executingSql;
         protected set { }
     }
-    
-
 }
-public class DiscouragedDirectSqlRequest:MeadowRequest<MeadowVoid,MeadowVoid>
+
+public class DiscouragedDirectSqlRequest : MeadowRequest<MeadowVoid, MeadowVoid>
 {
     private readonly string _sql;
-    
+
     public DiscouragedDirectSqlRequest(string sql) : base(false)
     {
         _sql = sql;
@@ -33,6 +70,4 @@ public class DiscouragedDirectSqlRequest:MeadowRequest<MeadowVoid,MeadowVoid>
         get => _sql;
         protected set { }
     }
-    
-
 }
