8968fd1e3a194eb7281ca42c0ac3199384f1ee75
Mani Moayedi <acidmanic.moayedi@gmail.com>
2024-09-24 01:00:12 +0330

    Update ReadAllSnippet to be flexible for builders generic type and entityType

--- Meadow/Scaffolding/Snippets/Builtin/SelectAllSnippet.cs
+++ Meadow/Scaffolding/Snippets/Builtin/SelectAllSnippet.cs
@@ -1,22 +1,25 @@
+using System;
 using Acidmanic.Utilities.Filtering;
 using Acidmanic.Utilities.Filtering.Models;
+using Acidmanic.Utilities.Filtering.Utilities;
 using Meadow.Contracts;
 using Meadow.Scaffolding.Extensions;
 using Meadow.Scaffolding.Models;
 
 namespace Meadow.Scaffolding.Snippets.Builtin;
 
-public class SelectAllSnippet<TEntity>:ISnippet
+public class SelectAllSnippet : ISnippet
 {
-
     private readonly FilterQuery _filterQuery;
     private readonly OrderTerm[] _orders;
     private readonly bool _usePagination;
     private readonly bool _fullTree;
     private readonly Parameter _offsetParameter = Parameter.Null;
     private readonly Parameter _sizeParameter = Parameter.Null;
-    
-    public SelectAllSnippet(FilterQuery filterQuery, OrderTerm[] orders, Parameter offset,Parameter size, bool fullTree)
+    private readonly Type _entityType;
+
+    public SelectAllSnippet(Type entityType, FilterQuery filterQuery, OrderTerm[] orders, Parameter offset,
+        Parameter size, bool fullTree)
     {
         _filterQuery = filterQuery;
         _orders = orders;
@@ -24,33 +27,95 @@ public class SelectAllSnippet<TEntity>:ISnippet
         _offsetParameter = offset;
         _sizeParameter = size;
         _fullTree = fullTree;
+        _entityType = entityType;
     }
-    
-    public SelectAllSnippet(FilterQuery filterQuery, OrderTerm[] orders, bool fullTree)
+
+    public SelectAllSnippet(Type entityType, FilterQuery filterQuery, OrderTerm[] orders, bool fullTree)
     {
         _filterQuery = filterQuery;
         _orders = orders;
         _fullTree = fullTree;
+        _entityType = entityType;
         _usePagination = false;
-        
     }
 
     public ISnippetToolbox Toolbox { get; set; } = ISnippetToolbox.Null;
 
 
+    private ISnippetToolbox T => Toolbox.CloneFor(_entityType);
+
     public string Pagination => _usePagination
-        ? Toolbox.SqlTranslator.TranslatePagination(_offsetParameter, _sizeParameter)
+        ? T.SqlTranslator.TranslatePagination(_offsetParameter, _sizeParameter)
         : string.Empty;
 
-    public string Order => Toolbox.SqlTranslator.TranslateOrders(Toolbox.EffectiveType, _orders, _fullTree);
+    public string Order => T.SqlTranslator.TranslateOrders(T.EffectiveType, _orders, _fullTree);
 
     public string Where =>
-        Toolbox.SqlTranslator.TranslateFilterQueryToDbExpression(_filterQuery,
+        T.SqlTranslator.TranslateFilterQueryToDbExpression(_filterQuery,
             ColumnNameTranslation.DataOwnerDotColumnName);
 
-    public string Source => Toolbox.SourceName();
+    public string Source => T.SourceName();
 
-    public string Semicolon => Toolbox.Semicolon();
-    
+    public string Semicolon => T.Semicolon();
+
     public string Template => "Select * FROM {Source} {Where} {Order} {Pagination}{Semicolon}";
+
+
+    public static SelectAllSnippet Create<TBuildersArgument>(
+        Type actualEntityType,
+        Parameter offset, Parameter size,
+        Action<FilterQueryBuilder<TBuildersArgument>>? filter = null,
+        Action<OrderSetBuilder<TBuildersArgument>>? order = null,
+        bool fullTree = false)
+    {
+        Action<FilterQueryBuilder<TBuildersArgument>> fb = filter ?? (b => { });
+        Action<OrderSetBuilder<TBuildersArgument>> ob = order ?? (o => { });
+
+
+        var filterBuilder = new FilterQueryBuilder<TBuildersArgument>();
+        var orderBuilder = new OrderSetBuilder<TBuildersArgument>();
+
+        fb(filterBuilder);
+        ob(orderBuilder);
+
+        var filterQuery = filterBuilder.Build();
+        var orders = orderBuilder.Build();
+
+        return new SelectAllSnippet(actualEntityType, filterQuery, orders, offset, size, fullTree);
+    }
+
+    public static SelectAllSnippet Create<TEntity>(
+        Parameter offset, Parameter size,
+        Action<FilterQueryBuilder<TEntity>>? filter = null,
+        Action<OrderSetBuilder<TEntity>>? order = null,
+        bool fullTree = false) => Create<TEntity>(typeof(TEntity),
+        offset, size, filter, order, fullTree);
+
+    public static SelectAllSnippet Create<TBuildersArgument>(
+        Type actualEntityType,
+        Action<FilterQueryBuilder<TBuildersArgument>>? filter = null,
+        Action<OrderSetBuilder<TBuildersArgument>>? order = null,
+        bool fullTree = false)
+    {
+        Action<FilterQueryBuilder<TBuildersArgument>> fb = filter ?? (b => { });
+        Action<OrderSetBuilder<TBuildersArgument>> ob = order ?? (o => { });
+
+
+        var filterBuilder = new FilterQueryBuilder<TBuildersArgument>();
+        var orderBuilder = new OrderSetBuilder<TBuildersArgument>();
+
+        fb(filterBuilder);
+        ob(orderBuilder);
+
+        var filterQuery = filterBuilder.Build();
+        var orders = orderBuilder.Build();
+
+        return new SelectAllSnippet(actualEntityType, filterQuery, orders, fullTree);
+    }
+
+    public static SelectAllSnippet Create<TEntity>(
+        Action<FilterQueryBuilder<TEntity>>? filter = null,
+        Action<OrderSetBuilder<TEntity>>? order = null,
+        bool fullTree = false)
+        => Create(typeof(TEntity), filter, order, fullTree);
 }
